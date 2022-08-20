74f6112a4a7ca6b9d0f1cea56f4b605ffcaf5aaf
stevewalsh1989 <stevewalsh89@gmail.com>
2022-08-20 20:39:16 +0100

    better svg line naming

--- src/components/CellsSVG.vue
+++ src/components/CellsSVG.vue
@@ -6,32 +6,32 @@
 
 import { computed, onMounted, onUpdated, ref } from 'vue';
 
-const value1 = ref(340);
-const value2 = ref(150);
+const leftValue = ref(340);
+const rightValue = ref(150);
 const svg = ref('#svg');
-const line1x1 = ref(0);
-const line1x2 = ref(0);
-const line2x1 = ref(0);
-const line2x2 = ref(0);
+const leftLinex1 = ref(0);
+const leftLinex2 = ref(0);
+const rightLinex1 = ref(0);
+const rightLinex2 = ref(0);
 
-const gap = value1.value === 0 || value2.value === 0 ? 0 : 4;
+const gap = leftValue.value === 0 || rightValue.value === 0 ? 0 : 4;
 
 // Percentage calculations
-const total = computed(() => value1.value + value2.value);
-const value1Percent = computed(() =>
-  Math.round((value1.value / total.value) * 100),
+const total = computed(() => leftValue.value + rightValue.value);
+const leftValuePercent = computed(() =>
+  Math.round((leftValue.value / total.value) * 100),
 );
-const value2Percent = computed(() =>
-  Math.round((value2.value / total.value) * 100),
+const rightValuePercent = computed(() =>
+  Math.round((rightValue.value / total.value) * 100),
 );
 
 // calculate new xy coordinate for svg lines
 function updatelines() {
   const svgWidth = svg.value.clientWidth;
-  line1x1.value = gap;
-  line1x2.value = svgWidth * (value1Percent.value / 100) - gap;
-  line2x1.value = svgWidth * (value1Percent.value / 100) + gap;
-  line2x2.value = svgWidth - gap;
+  leftLinex1.value = gap;
+  leftLinex2.value = svgWidth * (leftValuePercent.value / 100) - gap;
+  rightLinex1.value = svgWidth * (leftValuePercent.value / 100) + gap;
+  rightLinex2.value = svgWidth - gap;
 }
 
 onMounted(() => {
@@ -47,20 +47,20 @@ onUpdated(() => {
   <div class="w-52 box flex flex-col mx-auto" ref="svg">
     <div class="flex flex-col mb-12">
       <div class="flex mb-8 ml-4">
-        <label for="value1" class="mr-4">left value:</label>
+        <label for="leftValue" class="mr-4">left value:</label>
         <input
           type="number"
-          v-model.number="value1"
+          v-model.number="leftValue"
           max="999"
           min="0"
           class="w-16 bg-primary-white border-b-2 pl-2"
         />
       </div>
       <div class="flex mb-4 ml-4">
-        <label for="value2" class="mr-2">right value:</label>
+        <label for="rightValue" class="mr-2">right value:</label>
         <input
           type="number"
-          v-model.number="value2"
+          v-model.number="rightValue"
           max="999"
           min="0"
           class="w-16 bg-primary-white border-b-2 pl-2"
@@ -69,20 +69,20 @@ onUpdated(() => {
     </div>
     <svg height="12">
       <line
-        v-if="value1"
-        :x1="line1x1"
+        v-if="leftValue"
+        :x1="leftLinex1"
         :y1="3"
-        :x2="line1x2"
+        :x2="leftLinex2"
         :y2="3"
         stroke="#26a69a"
         stroke-linecap="round"
         stroke-width="6"
       />
       <line
-        v-if="value2"
-        :x1="line2x1"
+        v-if="rightValue"
+        :x1="rightLinex1"
         :y1="3"
-        :x2="line2x2"
+        :x2="rightLinex2"
         :y2="3"
         stroke="#b2dfdb"
         stroke-linecap="round"
@@ -90,8 +90,8 @@ onUpdated(() => {
       />
     </svg>
     <div class="flex mt-2">
-      <span class="ml-4 mr-auto">{{ value1Percent }}% </span>
-      <span class="mr-4 ml-auto">{{ value2Percent }}%</span>
+      <span class="ml-4 mr-auto">{{ leftValuePercent }}% </span>
+      <span class="mr-4 ml-auto">{{ rightValuePercent }}%</span>
     </div>
   </div>
 </template>
